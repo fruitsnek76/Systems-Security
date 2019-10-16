06314a2dcedb8acf886dc8510ee712c4e1417b5c
mikewolf256 <mikewolf256@gmail.com>
2019-10-16 19:18:31 -0400

    Push to linux

--- crypto_warmup.py
+++ crypto_warmup.py
@@ -1,5 +1,5 @@
 import os
-black = ""
+black = 1
 color = ""
 ciphers = ["aes-128-cbc",
 "aes-256-cbc",
@@ -22,6 +22,11 @@ ciphers = ["aes-128-cbc",
 "des-ecb",
 "des-ede3-ofb",]
 #Iterate through the list
-for x in ciphers:
-    os.system(openssl x black)
-    os.system(openssl x color)
+def check(color1,color2):
+    for x in ciphers:
+        #os.system(openssl x black)
+        #dd conv=notrunc if=/color.bmp of=color2.bmp bs=1 count=54
+        print("[+]", x)
+        os.system(openssl dd conv=notrunc if=color1 of=color1.bmp bs=1 count=54)
+        os.system(openssl dd conv=notrunc if=color2 of=color2.bmp bs=1 count=54)
+        #os.system(openssl x color)
